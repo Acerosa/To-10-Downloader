14ae03c4550189bc440c763ca5f25d66ddac7f69
Ricardo Rosa <rjrosa@outlook.com>
2020-09-25 12:37:28 +0100

    Committing the download and parsing of the data from the feed.

--- app/src/main/java/com/rr/top10downloader/MainActivity.kt
+++ app/src/main/java/com/rr/top10downloader/MainActivity.kt
@@ -4,85 +4,65 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.MalformedURLException
 import java.net.URL
 
+
+class EntryFeed{
+    var name:   String = ""
+    var artist: String = ""
+    var release: String = ""
+    var summury: String = ""
+    var imageURL: String = ""
+
+    override fun toString(): String {
+        return """
+            name = $name
+            artist = $artist
+            releaseDate = $release
+            summary = $summury
+            imageURL = $imageURL
+        """.trimIndent()
+    }
+}
+
 class MainActivity : AppCompatActivity() {
-    //This is a tag used for logging so we know the activity is refering to.
+
     private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //Logging the method call
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate called")
 
-        //Calling the inner class
+        Log.d(TAG, "onCreate called")
         val downloadData = DownloadData()
         downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
-        Log.d(TAG,"onCreate done")
+        Log.d(TAG, "onCreate: done")
     }
-    //Inner class to download the data
-    private inner class DownloadData: AsyncTask<String, Void, String>(){
-        private val TAG = "DownloadData"
 
-        // The function to download the xml feed
-        override fun doInBackground(vararg url: String?): String {
-            Log.d(TAG, "doInBackground: starts with ${url[0]}")
-            val rssFeed = downloadXML(url[0])
-            if (rssFeed.isEmpty()){
-                Log.e(TAG,  "doInBackground: Error downloading")
+    companion object {
+        private class DownloadData : AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadData"
+
+            override fun onPostExecute(result: String) {
+                super.onPostExecute(result)
+                //Log.d(TAG, "onPostExecute: parameter is $result")
+                val parseApplication = ParseApplication()
+                parseApplication.parse(result)
             }
-            return rssFeed
-        }
 
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-            Log.d(TAG, "onPostExecute: parameter is ${result} ")
-        }
-    }
-
-    // xml function download
-    private fun downloadXML(urlPath: String?) : String{
-        //turn the xml in to a string
-        val xmlResult = StringBuilder()
-
-        // Making the connection
-        try {
-            val url = URL(urlPath)
-            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-            val response  = connection.responseCode
-            Log.d(TAG, "downLoadXML: Thea response code is $response")
-
-//            val inputStream = connection.inputStream
-//            val inputStreamReader = InputStreamReader(inputStream)
-//            val   reader = BufferedReader(inputStreamReader)
-
-            //reading the connection response
-            val reader = BufferedReader(InputStreamReader(connection.inputStream))
-
-            val inputBuffer = CharArray(500)
-            var charsRead = 0
-            while (charsRead >= 0){
-                charsRead = reader.read(inputBuffer)
-                if (charsRead > 0){
-                    xmlResult.append(String(inputBuffer, 0, charsRead))
+            override fun doInBackground(vararg url: String?): String {
+                Log.d(TAG, "doInBackground: starts with ${url[0]}")
+                val rssFeed = downloadXML(url[0])
+                if (rssFeed.isEmpty()) {
+                    Log.e(TAG, "doInBackground: Error downloading")
                 }
+                return rssFeed
             }
-            reader.close()
-            Log.d(TAG, "Receiver: ${xmlResult.length} bytes")
-            return xmlResult.toString()
-        } catch (e: MalformedURLException){
-            Log.d(TAG, "downloadXML: Invalid URL ${e.message}")
-        }catch (e: IOException){
-            Log.d(TAG, "downloadXML: IO Exception reading data: ${e.message}")
-        }catch (e: IOException){
-            Log.d(TAG, "unknown error : ${e.message}")
+
+            private fun downloadXML(urlPath: String?): String {
+                return URL(urlPath).readText()
+            }
         }
-        return "" // If it get to here there as been a problem. Returning an empty String
+
     }
 }
---
+++ app/src/main/java/com/rr/top10downloader/ParseApplication.kt
@@ -0,0 +1,73 @@
+package com.rr.top10downloader
+
+import android.util.Log
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+
+class ParseApplication {
+    private val TAG = "ParseApplication"
+    val applications = ArrayList<EntryFeed>()
+
+    fun parse(xmlData: String): Boolean {
+        Log.d(TAG, "parse called with $xmlData")
+        var status = true
+        var inEntry = false
+        var textValue = ""
+
+        try {
+            val factory = XmlPullParserFactory.newInstance()
+            factory.isNamespaceAware = true
+            val xpp = factory.newPullParser()
+            xpp.setInput(xmlData.reader())
+            var eventType = xpp.eventType
+            var currentRecord = EntryFeed()
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                val tagName = xpp.name?.toLowerCase()
+                when (eventType) {
+
+                    XmlPullParser.START_TAG -> {
+                        Log.d(TAG, "parse: Starting tag for " + tagName)
+                        if (tagName == "entry") {
+                            inEntry = true
+                        }
+                    }
+
+                    XmlPullParser.TEXT -> textValue = xpp.text
+
+                    XmlPullParser.END_TAG -> {
+                        Log.d(TAG, "parse: Ending tag for " + tagName)
+                        if (inEntry) {
+                            when (tagName) {
+                                "entry" -> {
+                                    applications.add(currentRecord)
+                                    inEntry = false
+                                    currentRecord = EntryFeed()   // create a new object
+                                }
+
+                                "name" -> currentRecord.name = textValue
+                                "artist" -> currentRecord.artist = textValue
+                                "releasedate" -> currentRecord.release = textValue
+                                "summary" -> currentRecord.summury = textValue
+                                "image" -> currentRecord.imageURL = textValue
+                            }
+                        }
+                    }
+                }
+
+                // Nothing else to do.
+                eventType = xpp.next()
+            }
+
+            for (app in applications) {
+                Log.d(TAG,"*******************")
+                Log.d(TAG,app.toString())
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            status = false
+        }
+
+        return status
+    }
+}
